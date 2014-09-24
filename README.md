fe59b67fafbbfe64ab131d0d55ae09b4e9d7ea55
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2014-09-24 09:34:33 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-xwiki-build-script
+XWiki Enterprise Build Script
 ==================
 
-Easily build an XWiki instance
+Easily build a reasy-to-use XWiki instance with this helper.
+
+Objective
+===
+I often build an up-to-date version of [XWiki Enterprise](http://enterprise.xwiki.org/xwiki/bin/view/Main/WebHome) with maven. This script automates the process of creating the Jetty/HSQLDB distribution and unziping it.
+
+By default, the generated instance is unzipped in a directory not related to the XWiki Sources. The reason is that when I run XWiki in the standard maven target directory, my IDE tries to index the files and become very slow.
+
+Prerequisites
+===
+You need a Python interpreter, Maven ([correctly configured](http://dev.xwiki.org/xwiki/bin/view/Community/Building)), and the XWiki sources. The script assumes that you have cloned the (XWiki Enterprise Repository)[https://github.com/xwiki/xwiki-enterprise] in the `~/xwiki/xwiki-enterprise` directory. It will create the build in the `~/xwiki-instance` directory.
+
+Usage
+===
+Just do `python make-enterprise.py`.
+If a previous instance exists, it will be deleted.
+
+Change paths
+===
+If you want to change the path of the sources and/or of the output, just edit the script itself and change the global variables at the begining of the file (`xe_sources`, `instance_path`).
+
+Options
+===
+* `-U`: same as maven, it will force the update of the dependencies.
+* `-am`: (to be implemented) means 'Also Make dependencies'. Instead of downloading them, all dependencies that have their sources in the `xwiki-enterprise` directory will be compiled. So it does not concern dependencies like `xwiki-platform`, `xwiki-rendering` and `xwiki-commons`.
+
+Contact
+===
+Feel free to contact me, create issues or pull request on the github project. Thanks.
---
+++ make-enterprise.py
@@ -0,0 +1,84 @@
+#####################
+## IMPORTS
+#####################
+import os
+import os.path
+import shutil
+import zipfile
+import sys
+#####################
+## GLOBALS
+#####################
+home = os.path.expanduser("~")
+version = "6.3-SNAPSHOT"
+cmd = "mvn clean install -pl xwiki-enterprise-distribution/xwiki-enterprise-jetty/xwiki-enterprise-jetty-hsqldb/"
+xe_sources = home + "/xwiki/xwiki-enterprise"
+xe_target = xe_sources+ "/xwiki-enterprise-distribution/xwiki-enterprise-jetty/xwiki-enterprise-jetty-hsqldb/target"
+instance_path = home + "/xwiki-instance"
+#####################
+## PRINT LOG SECTION
+#####################
+def log_section(title):
+  nb = 120
+  color = '\033[95m'
+  normal_color = '\033[0m'
+  print color + "-" * nb
+  print title
+  print "-" * nb + normal_color
+#####################
+## BUILD
+#####################
+def build():
+  for arg in sys.argv:
+    if arg == "-U":
+      global cmd
+      cmd = cmd + " -U"
+    elif arg == "nobuild":
+      log_section("1. NO BUILD")
+      return
+  log_section("1. Build XWiki Enterprise (" + cmd + ")")
+  current_path = os.getcwd()
+  os.chdir(xe_sources)
+  os.system(cmd)
+  os.chdir(current_path)
+#####################
+## CLEAN
+#####################
+def clean():
+  log_section("2. Remove old build (" + instance_path + ")")
+  if os.path.exists(instance_path):
+    shutil.rmtree(instance_path)
+#####################
+## UNZIP
+#####################
+def unzip():
+  log_section("3. Uncompressing the new build")
+  zfile = zipfile.ZipFile(xe_target + "/xwiki-enterprise-jetty-hsqldb-"+version+".zip")
+  for name in zfile.namelist():
+    (dirname, filename) = os.path.split(name)
+    if dirname.startswith("xwiki-enterprise-jetty-hsqldb-" + version):
+      dirname = dirname[len("xwiki-enterprise-jetty-hsqldb-" + version)+1:len(dirname)]
+    dirname = instance_path + "/" + dirname
+    #print "Decompressing " + filename + " on " + dirname
+    if not os.path.exists(dirname):
+      os.makedirs(dirname)
+    if filename <> '':
+      ## not a directory
+      fd = open(dirname + "/" + filename, 'w')
+      fd.write(zfile.read(name))
+      fd.close()
+  zfile.close()
+  os.system("echo " + version + " > " + instance_path + "/VERSION")
+  os.system("ln -s " + instance_path + "/webapps/xwiki/WEB-INF/lib/ " + instance_path + "/lib")
+#####################
+## SCRIPT
+#####################
+## 1. Build
+build()
+## 2. Remove old build
+clean()
+## 3. Uncompressing the new build
+unzip()
+## 4. End
+log_section("Done")
+
